33310d09b4fe790bbef561237fe44a57ad0deafa
Aleksey Zvagolsky <alekseyzvagolsky@gmail.com>
2023-10-31 22:00:43 +0200

    init

---
+++ index.js
@@ -0,0 +1,103 @@
+import express from 'express';
+import axios from 'axios';
+import cors from 'cors'
+
+const PORT = process.env.PORT ?? 3000;
+const app = express();
+app.use(cors());
+const TOKEN = "6843300862:AAHH9LfxEbYnrxxNBRDBy54s661eCpCtI4c";
+const TELEGRAM_API_BASE_URL = `https://api.telegram.org/bot${TOKEN}`;
+// const ACCESS_TOKEN = "444901bb84e6dfdaa3";
+// const VK_API_VERSION = '5.131'; // Версия VK API
+// const GROUP_ID = 'howorld'; // Идентификатор группы ВКонтакте
+
+app.get("/", (req, res) => {
+  res.send('<h1>Здарова</h1>')
+});
+
+
+app.listen(PORT, () => {
+  console.log(`Server has been started on port ${PORT}`)
+});
+
+app.get("/getSubscribersTG", async (req, res) => {
+  try {
+    const response = await axios.get(
+      `${TELEGRAM_API_BASE_URL}/getChatMembersCount`,
+      {
+        params: {
+          chat_id: "@TheHolyWorld",
+        },
+      }
+    );
+
+    const subscribersCount = response.data.result;
+    res.json({ subscribersCount });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: "Failed to fetch subscribers count" });
+  }
+});
+
+// app.get("/getSubscribersVK", async (req, res) => {
+//   try {
+//     const response = await axios.get(
+//       "https://api.vk.com/method/groups.getById",
+//       {
+//         params: {
+//           access_token:
+//             "6c398c356c398c356c398c35aa6f2f920166c396c398c350907df77245b8e79d144f98b",
+//           group_id: "205337728",
+//           fields: "members_count",
+//           v: "5.154",
+//         },
+//       }
+//     );
+
+//     console.log(
+//       "------------------------------------------------------------------"
+//     );
+//     console.log(response);
+//     console.log(
+//       "------------------------------------------------------------------"
+//     );
+
+//     res.json(response.data);
+//   } catch (error) {
+//     console.log(
+//       "------------------------------------------------------------------"
+//     );
+//     console.log(error);
+//     console.log(
+//       "------------------------------------------------------------------"
+//     );
+//     res.status(500).json({ error: "Internal Server Error" });
+//   }
+// });
+
+const API_KEY_YOUTUBE = "AIzaSyAPuQJ7JDSPV-QJ-rYjdQvZYAi0zfZpFAw";
+const CHANNEL_ID = "UCFpPYQeIU-cUTf-uyRkg9Rw";
+
+app.get("/getSubscribersYouTube", async (req, res) => {
+  try {
+    const response = await axios.get(
+      'https://www.googleapis.com/youtube/v3/channels',
+      {
+        params: {
+          part: "statistics",
+          id: CHANNEL_ID,
+          key: API_KEY_YOUTUBE,
+        },
+      }
+    );
+
+    const subscribersCount = response.data.items[0].statistics.subscriberCount;
+
+    res.json({ subscribersCount });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: "Failed to fetch subscribers count" });
+  }
+});
+
+export default app
